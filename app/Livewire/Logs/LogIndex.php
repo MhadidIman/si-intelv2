113f48b9ff9d4a048accd7a17a085453c529bf8d
MhadidIMan <hadidmuhamad417@gmail.com>
2026-02-12 10:11:47 +0800

    up error

---
+++ app/Livewire/Logs/LogIndex.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Logs;
+
+use Livewire\Component;
+
+class LogIndex extends Component
+{
+    public function render()
+    {
+        return view('livewire.logs.log-index');
+    }
+}
